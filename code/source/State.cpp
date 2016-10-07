72ad7c77d5a4b7e336262b1b34b3f0236631aa2c
Pridexs <alehstk@gmail.com>
2016-10-07 19:53:52 -0300

    Initial Commit

--- code/source/State.cpp
+++ code/source/State.cpp
@@ -1,3 +1,16 @@
+/*
+ * This code is from projectM a 2D side scroller using SFML.
+ * It was made using the help of the SFML Game Development Book
+ * 
+ * If you have any questions, please contact me at
+ * 		pridexs.com
+ *
+ * For more information visit the repo for this project at:
+ * 		github.com/pridexs
+ *
+ * Alexandre Maros - 2016
+ */
+
 #include <State.h>
 
 #include <StateStack.h>
